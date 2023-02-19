b5e3978aed15754e15126deb35eb71e88d85eaaf
anas-dew <mailtoanas@duck.com>
2023-02-19 16:03:15 +0530

    new push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-      <Navbar heading="GPT"/>
+      <Navbar heading="Neural Mind AI"/>
       {/* <Sidebar/> */}
       <Chat/>
     </>
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,32 +1,46 @@
 import React from 'react'
 const Chat = () => {
-    var chatBox = document.getElementById('chat-box');
-    const AddToChat = () =>{
-        var chatBox = document.getElementById('chat-box');
+    const AddToChat = () => {
+        let chatBox = document.getElementById('chat-box');
         let currentText = document.getElementById('chat-input').value
         if (currentText.length <= 0) {
             chatBox.innerHTML = chatBox.innerHTML + "<div style='width:50%' class='p-2 m-4 bg-primary text-white rounded'>You input is empty. Feel free to ask me anything.</div>"
         } else {
-            chatBox.innerHTML =  chatBox.innerHTML + `<div style="width:50%" class='align-self-end text-end p-2 m-1 bg-success text-white rounded'>${currentText}</div>`
-            console.log('worked')
+            chatBox.innerHTML = chatBox.innerHTML + `<div style="width:50%" class='align-self-end text-end p-2 m-1 bg-success text-white rounded'></div>`
+            chatBox.lastChild.textContent = currentText
+            document.getElementById('chat-input').value = ""
+            chatBox.scrollTop = chatBox.scrollHeight;
         }
     }
     const ClearChat = () => {
+        let chatBox = document.getElementById('chat-box');
+        document.getElementById('chat-input').value = ""
         chatBox.innerHTML = "<div style='width:50%' class='p-2 m-4 bg-primary text-white rounded'>Hi, I am Neural Mind AI. You can ask me anything.</div>"
+
     }
-    // addToChat();
     return (
         <>
-        <div id='chat-box' className='mt-5 p-3 d-flex flex-column'>
-            <div className='p-2 m-4 bg-primary text-white rounded'>Hi, I am Neural Mind AI. You can ask me anything.</div>
-        </div>
-        <div>
-            <div className="d-flex p-3 fixed-bottom bg-white">
-                <button onClick={ClearChat} type="submit" className="rounded-pill btn btn-primary">Clear</button>
-                <input  type="text" className="rounded-pill form-control" placeholder='Ask me anything' id="chat-input"/>
-                <button onClick={AddToChat} id='send' type="submit" className="rounded-pill btn btn-primary">Send</button>
+            <div id='chat-box' style={{marginBottom: "5rem"}} className='mt-5 p-3 d-flex flex-column'>
+                <div style={{width: "50%"}} className='p-2 m-4 bg-primary text-white rounded'>Hi, I am Neural Mind AI. You can ask me anything.</div>
             </div>
-        </div>
+
+
+
+            {/* THESE ARE CHAT BUTTONS */}
+            <div>
+                <div className="d-flex p-3 fixed-bottom bg-white">
+                    <button onClick={ClearChat} type="submit" className="m-1 rounded-pill btn btn-primary"><i class="fa-solid fa-broom-wide"></i></button>
+                    <input onKeyDown={e => {
+                        if (e.keyCode === 13 && e.ctrlKey) {
+
+                        } else if (e.keyCode === 13) {
+                            AddToChat();
+                        }
+                    }}
+                        type="text" className="m-1 rounded-pill form-control" placeholder='Ask me anything' id="chat-input" />
+                    <button onClick={AddToChat} id='send' type="submit" className="m-1 rounded-pill btn btn-primary">Send</button>
+                </div>
+            </div>
         </>
     )
 }
